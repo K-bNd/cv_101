afbfc8f46ab96ad2ef6255fddc4e4d442ba3416f
KB <kevin-brian.n-diaye@epita.fr>
2025-04-18 15:51:36 +0200

    feat(resnet): add residual block

--- utils/conv_utils.py
+++ utils/conv_utils.py
@@ -4,8 +4,13 @@ import torch.nn as nn
 
 
 def create_conv_block(
-    in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: Union[int, str] = 0
+    in_channels: int,
+    out_channels: int,
+    kernel_size: int,
+    stride: int,
+    padding: Union[int, str] = 0,
 ) -> list[nn.Module]:
+    """Building block for Deep CNNs based on BatchNorm paper"""
     return [
         nn.Conv2d(
             in_channels=in_channels,
@@ -17,3 +22,23 @@ def create_conv_block(
         nn.BatchNorm2d(out_channels),
         nn.ReLU(),
     ]
+
+
+class ResidualBlock(nn.Module):
+    """Residual learning block from the ResNet paper"""
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        stride: int,
+        padding: Union[int, str] = 0,
+    ):
+        self.conv = nn.Sequential(
+            *create_conv_block(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+            *create_conv_block(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+        )
+    
+    def forward(self, x: torch.Tensor):
+        out = self.conv(x)
+        return out + x
