a85e0a9d56ff3d2cd134ad4dbc0dabb2dcc3c695
Yoku <59846141+K-bNd@users.noreply.github.com>
2025-04-22 15:52:19 +0200

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
 from .conv_utils import create_conv_block
-from .get_dataset import get_cifar10, get_imagenette, get_oxford
+from .get_dataset import get_cifar10, get_imagenette, get_oxford, get_mnist
