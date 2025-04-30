940c9528f99cb8c55673355dcd6f3bf269f1bccc
Kevin NDIAYE <kevin.ndiaye.ext@valeo.com>
2025-05-01 01:05:15 +0200

    feat(classification): add weight decay

--- task/classification.py
+++ task/classification.py
@@ -16,12 +16,14 @@ class BasicClassification(L.LightningModule):
         early_stopping_patience: int = 10,
         optimizer: str = "Adam",
         start_learning_rate: float = 1e-3,
+        weight_decay: float = 1e-5,
     ):
         super().__init__()
         self.num_classes = num_classes
         self.early_stopping_patience = early_stopping_patience
         self.optimizer = optimizer
         self.start_learning_rate = start_learning_rate
+        self.weight_decay = weight_decay
         self.accuracy = Accuracy(task="multiclass", num_classes=num_classes)
         self.preprocessing = None
         self.loss_fn = nn.CrossEntropyLoss()
@@ -77,7 +79,7 @@ class BasicClassification(L.LightningModule):
         return acc
 
     def configure_optimizers(self):
-        optimizer = getattr(optim, self.optimizer)(self.parameters(), lr=self.start_learning_rate)
+        optimizer = getattr(optim, self.optimizer)(self.parameters(), lr=self.start_learning_rate, weight_decay=self.weight_decay)
         return {
             "optimizer": optimizer,
             "lr_scheduler": {
