aeaab08543a17daa4765f72ca3af0004017e440e
KB <kevin-brian.n-diaye@epita.fr>
2026-03-16 20:51:14 +0100

    feat(config): add config for vits

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,16 @@
-from .model import ModelImplem
 from .basic_nn import BasicNN
-from .lenet import LeNet
-from .vgg16 import VGG16
-from .segnet import SegNet
-from .resnet import ResNet34, ResNet50
-from .cifar_resnet import cifar_resnet20, cifar_resnet32, cifar_resnet44, cifar_resnet56, cifar_resnet110, cifar_resnet1202
 from .bisenet_v2 import BiSeNetV2
+from .cifar_resnet import (
+    cifar_resnet20,
+    cifar_resnet32,
+    cifar_resnet44,
+    cifar_resnet56,
+    cifar_resnet110,
+    cifar_resnet1202,
+)
+from .lenet import LeNet
+from .model import ModelImplem
+from .resnet import ResNet34, ResNet50
+from .segnet import SegNet
+from .vgg16 import VGG16
+from .vit_5 import vit5_base, vit5_large, vit5_small, vit5_xlarge
